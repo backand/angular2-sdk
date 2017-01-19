72bdad2217a0ce2658ffe904ae91d3ae1d38a7bf
Ran Cohen <rannn505@outlook.com>
2017-01-20 00:58:41 +0200

    v1.0.5

--- src/backand.service.js
+++ src/backand.service.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var backand = require("@backand/bkndvanilla-sdk");
+var backand = require("@backand/vanilla-sdk");
 var BackandService = (function () {
     function BackandService() {
     }
--- src/backand.service.ts
+++ src/backand.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import * as backand from '@backand/bkndvanilla-sdk'
+import * as backand from '@backand/vanilla-sdk'
 
 @Injectable()
 export class BackandService {
